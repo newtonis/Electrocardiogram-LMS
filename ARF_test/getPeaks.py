6ee36f8631d990a7d11911bb0c045b6e7ea29fcd
taomasgonzalez <togonzalez@itba.edu.ar>
2019-11-09 16:15:15 -0300

    started with the IEEE paper format

--- ARF_test/getPeaks.py
+++ ARF_test/getPeaks.py
@@ -1,28 +1,33 @@
 import numpy as np
 
-# get peaks of signal
 
-"""
-min_distance: distance of peaks
-fs: frecuency sample
-signal: samples
-"""
+def getPeaks(signal, fs, min_distance, impulse_distance=0):
+    """
+    signal: samples
+    min_distance: distance of peaks
+    fs: sample frecuency
+    """
+    power_signal = np.power(signal, 2)
 
-def getPeaks(signal, fs, min_distance, impulse_distance = 0):
-    power_signal = np.power(signal,2)
-    max_value = max(power_signal) # maximo valor de la potencia
-    peakSignal = np.zeros(len(signal)) # señal donde almacenamos los picos
+    # Consideramos que la señal no variara considerablemente en al menos 10 periodos del filtro
+    stationarity_size = 10 * 500
+
+    max_value = max(power_signal[:stationarity_size])  # maximo valor de la potencia en el primer intervalo estacionario
+
+    peakSignal = np.zeros(len(signal))  # señal donde almacenamos los picos
 
     counter = 0
     impulse_ticks = int(impulse_distance * fs)
 
     for i in range(len(signal)):
+        if i % stationarity_size == 0:
+            max_value = max(power_signal[:stationarity_size])
+
         if counter > 0:
             counter -= 1
-        elif power_signal[i] > max_value * 0.5:
+        elif power_signal[i] > max_value * 0.43:
             counter = min_distance * fs
             if i > impulse_ticks:
                 peakSignal[i - impulse_ticks] = 1
 
     return peakSignal
-
--- ARF_test/test02filterTrain.py
+++ ARF_test/test02filterTrain.py
@@ -5,24 +5,25 @@ from scipy.signal import detrend
 from filterARF import filterARF
 import numpy as np
 
-### revisamos el funcionamiento de getPeaks
-
-## usamos la 202
 
 fs = 360
 timespan = 400
 time_cycle = 1.4
-
+# señal = no_noise_signal
 señal = all_signals["202"]["upper"][:timespan*fs]
 t = np.arange(len(señal)) / fs
-
 errores = list(all_signals["202"]["anomalies"].keys())
+errores = [float(errores[i]) / fs for i in range(len(errores))]
 
-for i in range(len(errores)):
-    errores[i] = float(errores[i]) / fs
 
-señal = detrend(señal)
+def anomaly_plotter(axis, anomalies, timespan, min, max):
+  for ai in anomalies:
+    if ai < timespan:
+        axis.plot([ai, ai], [min, max], 'orange')
 
+
+
+señal = detrend(señal)      # quitamos la tendencia que agrega el ruido de línea y de los movimientos corporales
 peaks = getPeaks(señal, fs, time_cycle / 2, time_cycle / 10)
 
 myFilterARF = filterARF(500, 0.1)
@@ -31,25 +32,13 @@ filtered, error = myFilterARF.train(peaks, señal)
 fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True)
 
 ax1.plot(t, señal)
-
-minv = min(señal)
-maxv = max(señal)
-
-for ei in errores:
-    if ei < timespan:
-        ax1.plot([ei, ei], [minv, maxv], 'orange')
+anomaly_plotter(axis=ax1, anomalies=errores, timespan=timespan, min=min(señal), max=max(señal))
 
 ax2.plot(t, peaks)
-
 ax3.plot(t, filtered)
 
-minv = min(error)
-maxv = max(error)
+anomaly_plotter(axis=ax4, anomalies=errores, timespan=timespan, min=min(error), max=max(error))
 
-for ei in errores:
-    if ei < timespan:
-        ax4.plot([ei, ei], [minv, maxv], 'orange')
-
-ax4.plot(t, error)
+ax4.plot(t, abs(error))
 
 plt.show()
